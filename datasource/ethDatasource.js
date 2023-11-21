19b68586dac0ff03f6b4994f8c744f0321ab0726
JamesOmare <jamesomare922@gmail.com>
2023-11-21 11:59:25 +0300

    Add files via upload

---
+++ datasource/ethDatasource.js
@@ -0,0 +1,46 @@
+const { RESTDataSource } = require("apollo-datasource-rest"); 
+
+// Vitalik's Ethereum Address
+const eth_address = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045";
+
+// Etherscan Data Source Class
+class EtherDataSource extends RESTDataSource {
+
+  constructor() {
+    super();
+    this.baseURL = "https://api.etherscan.io/api"; 
+  }
+
+  async etherBalanceByAddress() {
+    // Get ether balance for a specific address
+    return this.get(
+      `?module=account&action=balance&address=${eth_address}&tag=latest&apikey=${process.env.ETHERSCAN_API}`
+    );
+  }
+
+  async totalSupplyOfEther() {
+    // Get total ether supply
+    return this.get(  
+      `?module=stats&action=ethsupply&apikey=${process.env.ETHERSCAN_API}`
+    );
+  }
+
+  // New API Endpoints
+
+  async getLatestEthereumPrice() {
+    // Get the latest Ethereum price in USD
+    return this.get(
+      `?module=stats&action=ethprice&apikey=${process.env.ETHERSCAN_API}`
+    );
+  }
+
+  async getBlockConfirmationTime() {
+    // Get the estimated number of block confirmations for a transaction 
+    // with the specified gas price
+    return this.get(
+      `?module=gastracker&action=gasestimate&gasprice=2000000000&apikey=${process.env.ETHERSCAN_API}`
+    );
+  }
+}
+
+module.exports = EtherDataSource;
